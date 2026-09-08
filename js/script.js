4f161f9d59ab1489741a2e7e4d93e8de8cc6405e
9Apostrophe9 <mittsu2002022032104@gmail.com>
2026-09-08 18:17:42 +0900

    スマホ修正①に戻し

--- js/script.js
+++ js/script.js
@@ -3,25 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuButton = document.querySelector(".menu-button");
     const navigation = document.querySelector(".navigation");
 
-    if (!menuButton || !navigation) return;
-
-    // MENUを押したとき
     menuButton.addEventListener("click", function () {
 
         navigation.classList.toggle("menu-open");
-
-    });
-
-    // メニュー内のリンクを押したとき
-    const menuLinks = navigation.querySelectorAll("a");
-
-    menuLinks.forEach(function (link) {
-
-        link.addEventListener("click", function () {
-
-            navigation.classList.remove("menu-open");
-
-        });
 
     });
 
