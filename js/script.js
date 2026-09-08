5c2a043afcf813bf5cc8e5cb2e8e7f879bf12c0e
9Apostrophe9 <mittsu2002022032104@gmail.com>
2026-09-08 18:34:26 +0900

    MENU閉じる

--- js/script.js
+++ js/script.js
@@ -2,11 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const menuButton = document.querySelector(".menu-button");
     const navigation = document.querySelector(".navigation");
+    const navigationLinks = document.querySelectorAll(".navigation a");
 
+    // MENUを開閉
     menuButton.addEventListener("click", function () {
-
         navigation.classList.toggle("menu-open");
+    });
 
+    // メニュー内のリンクを選択したらMENUを閉じる
+    navigationLinks.forEach(function (link) {
+        link.addEventListener("click", function () {
+            navigation.classList.remove("menu-open");
+        });
     });
 
 });
